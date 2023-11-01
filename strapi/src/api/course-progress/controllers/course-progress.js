c8923a2bfe5b97c9e02915f9e6bf5539a0871335
Raghav Jindal <raghavjindal0212@gmail.com>
2023-11-02 00:48:32 +0530

    added strapi cms

--- strapi/src/api/course-progress/controllers/course-progress.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * course-progress controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::course-progress.course-progress');
