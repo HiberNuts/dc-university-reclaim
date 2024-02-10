9a77e766351eb17bb4f68a8b1cf8ed516facf2e2
Raghav Jindal <raghavjindal0212@gmail.com>
2024-02-10 13:39:37 +0530

    Added dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,19 +5,20 @@ import { compression } from "vite-plugin-compression2";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), compression()],
-  define: {
-    "process.env": {},
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        // Set to true to enable code splitting
-        manualChunks: {
-          // Define your entry points and the chunks you want to create
-          // Here, 'vendor' is a common name for external dependencies
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
-  },
+  // define: { 'process.env.NODE_ENV': '"production"' }
+  // define: {
+  //   "process.env": {},
+  // },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       // Set to true to enable code splitting
+  //       manualChunks: {
+  //         // Define your entry points and the chunks you want to create
+  //         // Here, 'vendor' is a common name for external dependencies
+  //         vendor: ['react', 'react-dom'],
+  //       },
+  //     },
+  //   },
+  // },
 });
