ae5c7c391d7fd8631b619984d92cc3a5ed24397c
ebharathi <elayabharathi10102002@gmail.com>
2024-05-31 01:21:51 +0530

    added past contests

--- client/src/components/Contest/List/ContestList.jsx
+++ client/src/components/Contest/List/ContestList.jsx
@@ -1,8 +1,124 @@
+import { motion, useScroll } from "framer-motion";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { useRef } from "react";
+import Carousel from "../../Carousel/Carousel"
+import ContestCard from '../Card';
+import GreenButton from "../../button/GreenButton";
+import CONTEST_IMG from '../../../assets/contest.png';
+import CALENDER from '../../../assets/calendar_month.png';
+const PAST_CONTESTS=[
+    {
+        img:CONTEST_IMG,
+        title:"Contest Name",
+        description:"Ensure rapid development and build powerful Linearly Scalable Dapps with Shardeum Ensure",
+        endDate:"May 21, 2024 7:30AM (GMT+5:30)"
+    },
+    {
+        img:CONTEST_IMG,
+        title:"Contest Name",
+        description:"Ensure rapid development and build powerful Linearly Scalable Dapps with Shardeum Ensure",
+        endDate:"May 21, 2024 7:30AM (GMT+5:30)"
+    },
+    {
+        img:CONTEST_IMG,
+        title:"Contest Name",
+        description:"Ensure rapid development and build powerful Linearly Scalable Dapps with Shardeum Ensure",
+        endDate:"May 21, 2024 7:30AM (GMT+5:30)"
+    },
+    {
+        img:CONTEST_IMG,
+        title:"Contest Name",
+        description:"Ensure rapid development and build powerful Linearly Scalable Dapps with Shardeum Ensure",
+        endDate:"May 21, 2024 7:30AM (GMT+5:30)"
+    },
+    {
+        img:CONTEST_IMG,
+        title:"Contest Name",
+        description:"Ensure rapid development and build powerful Linearly Scalable Dapps with Shardeum Ensure",
+        endDate:"May 21, 2024 7:30AM (GMT+5:30)"
+    },
+    {
+        img:CONTEST_IMG,
+        title:"Contest Name",
+        description:"Ensure rapid development and build powerful Linearly Scalable Dapps with Shardeum Ensure",
+        endDate:"May 21, 2024 7:30AM (GMT+5:30)"
+    }
+]
 export default function ContestList()
 {
+    const slides = [
+        <ContestCard key={0} />,
+        <ContestCard key={1} />,
+        <ContestCard key={2} />
+        // Add more slide components as needed
+    ];
+      const scrollRef = useRef(null);
+      const { scrollYProgress } = useScroll({ target: scrollRef, offset: ["0 3", "1 1"] });
+     
     return(
         <div>
-            contest list
+            <div className="corousel-container bg-shardeumBlue min-h-[500px] py-10 sm:py-0 flex justify-center items-center">
+                <div className="h-[100%] w-full">
+                    <Carousel slides={slides}/>
+                </div>
+            </div>
+            <div className="past-contents-container bg-[#CAFFEF] min-h-[300px] pt-5 md:pt-20 px-5 md:px-40">
+                <div className="flex items-center my-2">
+                    <div className="flex-grow border-t border-gray-300"></div>
+                    <span className="mx-4 text-[40px] leading-tight font-helvetica-neue-bold text-shardeumBlack">Past Contests</span>
+                    <div className="flex-grow border-t border-gray-300"></div>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 md:grid-cols-3"> 
+                    {
+                        PAST_CONTESTS.map((single)=>
+                       <motion.div
+                        ref={scrollRef}
+                        style={{
+                        scale: scrollYProgress,
+                        opacity: scrollYProgress,
+                        boxShadow: "0px 4px 20px 0px rgba(195, 200, 255, 0.30)",
+                        }}
+                        className="flex flex-col card-container space-y-3 border-2 rounded-lg bg-white py-3 px-5 my-10 mx-5 shadow rounded-[20px] "
+                        >
+                               <div className="">
+                                     <LazyLoadImage
+                                        className="w-full  rounded-[16px]"
+                                        src={single.img}   
+                                        />
+                                </div>
+                                <div>
+                                    <p className="text-[32px] leading-tight text-overflow-ellipsis font-helvetica-neue-bold font-bold">
+                                        {single.title}
+                                    </p>
+                                </div>
+                                <div>
+                                    <p className="text-[16px] mt-2 text-slategray font-helvetica-neue-roman leading-[25px]">
+                                      {single.description}
+                                    </p>
+                                </div>
+                                <div>
+                                    <p className="text-[15px]">
+                                        <span className="pr-2">
+                                          <span className="leading-tight text-overflow-ellipsis font-helvetica-neue-bold">
+                                            {/* <LazyLoadImage
+                                                className="w-full  rounded-[16px]"
+                                                src={CALENDER}   height={"5px"} width={5}
+                                                /> */}
+                                          </span>
+                                          <span className="text-[14px]">
+                                             May 21, 2024 7:30AM (GMT+5:30)
+                                          </span> 
+                                        </span>
+                                    </p>
+                                </div>
+                                <div>
+                                     <GreenButton text={"View Solution"}/>
+                                </div>
+                      </motion.div> 
+                )
+                    }
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/components/Home/Contest.jsx
+++ client/src/components/Home/Contest.jsx
@@ -1,92 +1,12 @@
-import { useRef } from "react"
-import { motion, useScroll } from "framer-motion";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-
-import CONTEST_IMG from '../../assets/contest.png';
-import GreenButton from "../button/GreenButton";
+import ContestCard from "../Contest/Card"
 
 export default function Contest(){
-  const scrollRef = useRef(null);
-  const { scrollYProgress } = useScroll({ target: scrollRef, offset: ["0 3", "1 1"] });
-  let contentDescription="Ensure rapid development and build powerful Linearly Scalable Dapps with Shardeum Ensure rapid development and build powerful Linearly rapid development and build powerful Linearly";
-   return(
+  return(
     <div className="w-full mt-4 flex flex-col bg-shardeumWhite p-[12px] sm:p-[80px] text-black items-center  justify-center align-middle">
         <div className="flex flex-col w-full space-y-12">
            <p className="font-helvetica-neue-bold text-[64px]  items-center text-center  ">Upcoming Contest</p>
            <div className="flex justify-center items-center">
-           <motion.div
-            ref={scrollRef}
-            style={{
-            scale: scrollYProgress,
-            opacity: scrollYProgress,
-            boxShadow: "0px 4px 20px 0px rgba(195, 200, 255, 0.30)",
-            }}
-            className="flex card-container lg:h-[320px] bg-white border-[2px] shadow flex-col sm:flex-row justify-center align-middle w-full sm:w-[1200px] rounded-[16px] "
-            >
-                   <div className="flex-[2]">
-                        <LazyLoadImage
-                         className="w-full px-10 pt-7 rounded-[16px]"
-                         src={CONTEST_IMG}   
-                        />
-                   </div>                    
-                   <div className="flex-[3] p-4 sm:p-8">
-                        <div className="title-container">
-                            <p className="text-[32px] leading-tight text-overflow-ellipsis font-helvetica-neue-bold font-bold">
-                                How do you clone a smart contract?
-                            </p>
-                        </div>
-                        <div className="description-container">
-                            <p className="text-[16px] mt-2 text-slategray font-helvetica-neue-roman leading-[25px]">
-                                {contentDescription.slice(0, 140) + (contentDescription.length > 140 ? "..." : "")}
-                            </p>
-                        </div>
-                        <div className="details-container my-3">
-                            <div className="my-1">
-                                 <div className="">
-                                     <p className="text-[15px]">
-                                        <span className="pr-2 border-r-2 border-r-[#C3C8FF]">
-                                          <span className="leading-tight text-overflow-ellipsis font-helvetica-neue-bold">Difficulty Level:</span><span className="text-[14px]">  Easy</span> 
-                                        </span>
-                                        <span className="px-2">
-                                          <span className="leading-tight text-overflow-ellipsis font-helvetica-neue-bold">Participants:</span> <span className="text-[14px]">  100</span> 
-                                        </span>
-                                    </p>
-                                 </div>
-                            </div>
-                            <div className="my-1">
-                                 <div className="">
-                                     <p className="text-[15px]">
-                                        <span className="pr-2 border-r-2  border-r-[#C3C8FF]">
-                                          <span className="leading-tight text-overflow-ellipsis font-helvetica-neue-bold">Start:</span><span className="text-[14px]">  May 21, 2024 7:30AM (GMT+5:30)</span>
-                                        </span>
-                                        <span className="px-2">
-                                          <span className="leading-tight text-overflow-ellipsis font-helvetica-neue-bold">End:</span><span className="text-[14px]">  May 21, 2024 7:30AM (GMT+5:30)</span>
-                                        </span>
-                                    </p>
-                                 </div>
-                            </div>
-                            {/* When the contest is live  */}
-                            {/* <div className="my-1">
-                                 <div className="">
-                                     <p className="text-[15px]">
-                                       <span className="leading-tight text-overflow-ellipsis font-helvetica-neue-bold">Ending in:</span>
-                                     </p>
-                                 </div>
-                                 <div>
-                                      <p className="text-[14px]">
-                                        <span className="text-red-500">2d : 11h :59m :22s</span>
-                                      </p>
-                                 </div>
-                            </div> */}
-                        </div>
-                        <div className="button-container">
-                               <GreenButton
-                                 text={"Register Now"}
-                               />
-                        </div>
-                   </div>                    
- 
-            </motion.div>
+                <ContestCard></ContestCard>
            </div>
         </div>
     </div>
