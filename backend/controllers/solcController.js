d1a6e4f74d4872d1bdaf7a88bc0679e049cd370b
Mohamed Nasim <mohamed.nasim@onebillsoftware.com>
2024-06-11 22:48:56 +0530

    solcApi

--- backend/controllers/solcController.js
+++ backend/controllers/solcController.js
@@ -19,17 +19,17 @@ exports.compiler = async (req, res) => {
         }
       };
       
-    //   var output = JSON.parse(solc.compile(JSON.stringify(input)));
-    //   for (var contractName in output.contracts['test.sol']) {
-    //     console.log(
-    //       contractName +
-    //         ': ' +
-    //         output.contracts['test.sol'][contractName].evm.bytecode.object
-    //     );
-    //   }
-      res.send("compiled ")
+      var output = JSON.parse(solc.compile(JSON.stringify(input)));
+      for (var contractName in output.contracts['test.sol']) {
+        console.log(
+          contractName +
+            ': ' +
+            output.contracts['test.sol'][contractName].evm.bytecode.object
+        );
+      }
+      res.send(output)
     } catch (error) {
-      console.error("Error while fetching user progress", error);
+      console.error("Error while compiling", error);
       res.status(500).send({ message: error.message || "Internal Server Error" });
     }
   };
