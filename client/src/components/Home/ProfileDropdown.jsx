6bf1e345f21fe6c73105043cd1a46aca3595b0c1
ebharathi <elayabharathi10102002@gmail.com>
2024-07-04 22:06:04 +0530

    profile user image

--- client/src/components/Home/ProfileDropdown.jsx
+++ client/src/components/Home/ProfileDropdown.jsx
@@ -11,12 +11,17 @@ const ProfileDropDown = ({ account, openAccountModal, toggleNavbar, loggedInUser
         <div>
                  <Menu.Button>
         <div className={`flex gap-4 border-2 rounded-[24px] px-5 py-2 cursor-pointer ${homeRoute?'text-white':'text-black'}`}>
-                  <div className="flex gap-2">
+                                  <div className="flex gap-2">
                                       <img src={HEADER_XP} />
                                       <span className="pt-[2px]">{xp} XP</span>
                                   </div>
                                   <div className="flex gap-2">
-                                      <img src={HEADER_USER}/>
+                                     {
+                                      loggedInUserData?.image?
+                                       <img className="w-7 h-7 rounded-[50%]" src={loggedInUserData?.image??HEADER_USER}/>
+                                      :
+                                       <img src={HEADER_USER}/>
+                                     }
                                      {
                                       loggedInUserData?.shardId&&
                                        <span className="pt-[2px]">{loggedInUserData?.shardId?.slice(0, 13) + (loggedInUserData?.shardId?.length > 13 ? ".." : "")}</span>
--- client/src/components/Profile/ProfileProjectCard.jsx
+++ client/src/components/Profile/ProfileProjectCard.jsx
@@ -1,12 +1,12 @@
 const ProfileProjectCard=(props)=>{
-    return <div className="h-[275px] border rounded-[20px] w-[100%] col-span-1 cursor-pointer hover:w-[105%] transition-width duration-1000 ease-in-out" style={{boxShadow: "6px 6px 0px 0px #00000026"}}>
+    return <div className="h-[275px] overflow-y-auto border rounded-[20px] w-[100%] col-span-1 cursor-pointer hover:w-[105%] transition-width duration-1000 ease-in-out" style={{boxShadow: "6px 6px 0px 0px #00000026"}}>
 
         <div className="w-full h-[128px] rounded-t-[20px] bg-[#FF4C0F] border-b-[2px]">
 
         </div>
         <div className="py-[16px] px-[20px]">
             <p className="text-[18px] font-bold">{props.title}</p>
-            <p className="text-[16px] leading-[28px] font-[400]">{props.description}</p>
+            <p className="text-[16px] leading-[28px] font-[400] ">{props.description}</p>
         </div>
 
     </div>
