e936c9e625a6be25d118b8601e4c0e5c2d625090
mdnasim <mohamednasim3108@gmail.com>
2024-07-09 21:58:04 +0530

    loggedInuserData.image undefined erro fix

--- client/src/components/Profile/Edit.jsx
+++ client/src/components/Profile/Edit.jsx
@@ -1,6 +1,5 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import toast, { Toaster } from "react-hot-toast";
-
 import GreenButton from "../button/GreenButton";
 import {IMG_UPLOAD,REMOVE,TWITTER_PNG as TWITTER,GITHUB_PNG as GITHUB,YOUTUBE,LINKEDIN,DISCORD_PNG as DISCORD,DRAWER as HAMBURGER} from "../../Constants/Assets"
 import { useContext, useState } from "react";
@@ -141,7 +140,6 @@ const EditProfile = () => {
     }, {})
     console.log(filteredData)
     if (img) {
-      const userImageKey=loggedInUserData.image.substring(loggedInUserData.image.lastIndexOf('/') + 1)
       const formData = new FormData();
       formData.append("files", img);
       const res = await fetch(`${import.meta.env.VITE_CMS_URL}/upload?populate=*`, {
@@ -152,8 +150,10 @@ const EditProfile = () => {
         .then(res => res.json())
         .catch((error) => console.error(error));
       url = res[0].url
-
-      axios.post(`${import.meta.env.VITE_BACKEND_URL}/user/deleteImage`,{key:userImageKey})
+      if(loggedInUserData.image){
+        const userImageKey=loggedInUserData.image.substring(loggedInUserData.image.lastIndexOf('/') + 1)
+        axios.post(`${import.meta.env.VITE_BACKEND_URL}/user/deleteImage`,{key:userImageKey})
+      }
     }
 
     axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/updateuser`,
