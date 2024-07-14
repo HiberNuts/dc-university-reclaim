9c36d440399046c1a07ec92a888b80dcbfbd88cd
Raghav Jindal <raghavjindal0212@gmail.com>
2024-07-14 13:30:00 +0530

    added v3 version of testing api

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -2,5 +2,25 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.24",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      gas: 12000000,
+      blockGasLimit: 0x1fffffffffffff,
+      allowUnlimitedContractSize: true,
+      timeout: 1800000,
+      mining: {
+        auto: true,
+        interval: 0
+      }
+
+    },
+  },
+  // networks: {
+  //   goerli: {
+  //     url: `${process.env.ALCHEMY_GOERLI_URL}`,
+  //     accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`],
+  //   }
+  // }
 };
