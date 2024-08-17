048d8f35939e0db0120f22d31c97f62b01855326
Raghav Jindal <raghavjindal0212@gmail.com>
2024-08-17 21:01:54 +0530

    added coming soon

--- client/src/components/Home/Main.jsx
+++ client/src/components/Home/Main.jsx
@@ -1,11 +1,13 @@
 import React, { Suspense, lazy } from "react";
 import Unique from "./Unique";
+import img1 from "../../assets/soonBG.svg"
+import img2 from "../../assets/circle.svg"
 const Hero = lazy(() => import("./Hero"));
 const Community = lazy(() => import("./Community"));
 const FeatureCourses = lazy(() => import("./FeatureCourses"));
 const FaqHome = lazy(() => import("./FaqHome"));
 const Stats = lazy(() => import("./Stats"));
-const Contest = lazy(() => import('./Contest'))
+// const Contest = lazy(() => import('./Contest'))
 
 export default function Main() {
   return (
@@ -16,7 +18,14 @@ export default function Main() {
         <Hero />
         <Stats />
         <FeatureCourses />
-        <Contest />
+        <div className="w-full mt-4 flex flex-col bg-shardeumWhite p-[12px] sm:p-[80px] text-black items-center  justify-center align-middle">
+          <div className="relative w-full">
+            <img src={img1} alt="Background" className="hidden sm:block w-full" />
+            <img src={img2} alt="Background" className="sm:hidden w-full" />
+
+          </div>
+        </div>
+        {/* <Contest /> */}
         <Unique />
 
         <Community />
