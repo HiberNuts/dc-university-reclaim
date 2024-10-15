d4ac3e97c5d79213aa044e333d272e00f0186ae0
Hrithik <hrithik@gyangrove.co>
2024-10-16 01:10:19 +0530

    contest details page updated

--- client/src/components/Contest/Regsiter/Register.jsx
+++ client/src/components/Contest/Regsiter/Register.jsx
@@ -11,6 +11,8 @@ import { getContestByTitle, registerContest, alreadyRegistered, getLeaderboard }
 import ContestDetailsLoader from "../ContestLoaders/ContestDetailsLoader";
 import { renderContent } from "../../../utils/mapRichText";
 import DCButton from "../../button/DCButton";
+import cube from "../../../assets/cube.png";
+import line from "../../../assets/line.svg";
 
 
 export default function ContestRegsiter() {
@@ -100,55 +102,57 @@ export default function ContestRegsiter() {
       <Toaster />
       <div className="contest-header grid grid-cols-1 md:grid-cols-2 px-5 sm:px-10 md:px-[50px] lg:px-[100px] py-[50px] bg-black">
         <div className='order-2 md:order-1 pr-2 relative'>
+
+          <div className="size-[400px] rounded-full bg-[#3A59FE] absolute top-0 left-1/2 z-0 blur-[100px] opacity-40"></div>
           <div className="relative text-left w-fit mt-[-1.00px] 
-          bg-gradient-to-r from-[#ffffff] to-[#79797b] bg-clip-text text-wrap font-orbitron font-bold text-transparent text-[30px] tracking-[0] leading-[50px] whitespace-nowrap">
+          bg-gradient-to-r from-[#ffffff] to-[#79797b] bg-clip-text text-wrap font-orbitron font-bold text-transparent text-[40px] tracking-[0] leading-[50px] whitespace-nowrap">
             {contest?.title}
           </div>
-          <p className="relative self-stretch font-gilroy text-[#b1b0b9] text-base tracking-[0] leading-7">
+          <p className="relative self-stretch font-gilroy text-[#b1b0b9] text-[18px] tracking-[0] leading-7 mt-5">
             {contest.description}
           </p>
-          <div className="grid grid-cols-2 my-5">
+          <div className="grid grid-cols-2 mt-7 mb-9">
             {
               !timeLeft.status &&
               <div className="flex flex-col">
-                <p className="relative self-stretch font-semibold text-[#b1b0b9] text-[12px] tracking-[0] leading-7">
+                <p className="relative self-stretch  text-[14px] tracking-[0] leading-7 font-gilroybold text-[#B7C2FD]">
                   START:
                 </p>
-                <p className="relative self-stretch font-gilroy text-[#b1b0b9] text-base tracking-[0] leading-7">
+                <p className="relative self-stretch font-gilroy text-[#b1b0b9] text-[16px] tracking-[0] leading-7">
                   {formatTimestamp(contest.startDate)}</p>
               </div>
             }
             <div className="flex flex-col">
-              <p className="relative self-stretch font-semibold text-[#b1b0b9] text-[12px] tracking-[0] leading-7">
+              <p className="relative self-stretch  text-[14px] tracking-[0] leading-7 font-gilroybold text-[#B7C2FD]">
                 DIFFICULTY LEVEL:
               </p>
-              <p className="relative self-stretch font-gilroy text-[#b1b0b9] text-base tracking-[0] leading-7">
+              <p className="relative self-stretch font-gilroy text-[#b1b0b9] text-[16px] tracking-[0] leading-7">
                 {contest.level}</p>
             </div>
             {
               !timeLeft.status &&
               <div className="flex flex-col">
-                <p className="relative self-stretch font-semibold text-[#b1b0b9] text-[12px] tracking-[0] leading-7">
+                <p className="relative self-stretch  text-[14px] tracking-[0] leading-7 font-gilroybold text-[#B7C2FD]">
                   END:
                 </p>
-                <p className="relative self-stretch font-gilroy text-[#b1b0b9] text-base tracking-[0] leading-7">
+                <p className="relative self-stretch font-gilroy text-[#b1b0b9] text-[16px] tracking-[0] leading-7">
                   {formatTimestamp(contest.endDate)}</p>
               </div>
             }
             <div className="flex flex-col">
-              <p className="relative self-stretch font-semibold text-[#b1b0b9] text-[12px] tracking-[0] leading-7">
+                <p className="relative self-stretch  text-[14px] tracking-[0] leading-7 font-gilroybold text-[#B7C2FD]">
                 PARTICIPANTS:
               </p>
-              <p className="relative self-stretch font-gilroy text-[#b1b0b9] text-base tracking-[0] leading-7">
+              <p className="relative self-stretch font-gilroy text-[#b1b0b9] text-[16px] tracking-[0] leading-7">
                 {contest.participants}</p>
             </div>
             {
               timeLeft.status &&
               <div className="flex flex-col">
-                <p className="relative self-stretch font-semibold text-[#b1b0b9] text-[12px] tracking-[0] leading-7">
+                <p className="relative self-stretch  text-[14px] tracking-[0] leading-7 font-gilroybold text-[#B7C2FD]">
                   ENDS IN:
                 </p>
-                <p className="relative self-stretch font-gilroy text-[#b1b0b9] text-base tracking-[0] leading-7">
+                <p className="relative self-stretch font-gilroy text-[#b1b0b9] text-[16px] tracking-[0] leading-7">
                   {timeLeft?.timeleft}</p>
               </div>
             }
@@ -171,7 +175,7 @@ export default function ContestRegsiter() {
         </div>
         <div className='order-1 md:order-2 flex justify-center md:justify-end items-center'>
           <LazyLoadImage
-            className="h-[375px] py-2 rounded-[20px]"
+            className="h-[375px] py-2 rounded-[20px] z-10"
             src={contest?.image}
           />
         </div>
@@ -187,30 +191,49 @@ export default function ContestRegsiter() {
 
         </div>
       }
-      <div className="contest-details grid grid-cols-2 lg:grid-cols-3 px-5 sm:px-10 md:px-[50px] lg:px-[100px] py-[30px]">
-        <div className='col-span-2 md:pr-8'>
-          <div className='contest-details-title'>
+      <div className="contest-details relative grid grid-cols-2 lg:grid-cols-3 px-5 sm:px-10 md:px-[50px] lg:px-[100px] py-[30px] ">
+      <div className="absolute top-16 right-40">
+            <LazyLoadImage
+              className="h-[218px] w-[200px] py-2 rounded-[20px] z-10"
+              src={cube}
+            />
+          </div>
+          <div className="absolute top-1/2 right-[200px] blur-[3px] ">
+            <LazyLoadImage
+              className="h-[118px] w-[109px] rotate-[-180deg] z-10"
+              src={cube}
+            />
+          </div>
+          <div className="absolute top-0 right-0">
+            <LazyLoadImage
+              className=" py-2 z-10"
+              src={line}
+            />
+          </div>
+        <div className='col-span-2 md:pr-8 '>
+          
+          <div className='contest-details-title mb-5'>
             <div className="relative text-left w-fit mt-[-1.00px] 
           bg-gradient-to-r from-[#ffffff] to-[#79797b] bg-clip-text text-wrap font-orbitron font-bold text-transparent text-[30px] tracking-[0] leading-[50px] whitespace-nowrap">
               Contest Details
             </div>
           </div>
-          <div className='contest-details-description'>
+          <div className='contest-details-description mb-5'>
             <p className="relative self-stretch font-gilroy text-[#b1b0b9] text-base tracking-[0] leading-7">
               {contest?.details}
             </p>
           </div>
-          <div className='contest-details-rules py-5'>
-            <p className="relative self-stretch font-semibold text-[#b1b0b9] text-[12px] tracking-[0] leading-7">RULES:</p>
+          <div className='contest-details-rules py-5 mb-5'>
+            <p className="relative self-stretch font-gilroybold text-[#B7C2FD] text-[18px] tracking-[0] leading-7 mb-3">RULES:</p>
             <ul className='mx-1'>
               {contest.rules.map((item, index) => (
                 <React.Fragment key={index}>{renderContent(item)}</React.Fragment>
               ))}
             </ul>
           </div>
-          <div className='contest-details-rules py-5'>
-            <p className="relative self-stretch font-semibold text-[#b1b0b9] text-[12px] tracking-[0] leading-7">WINNINGS:</p>
-            <ul className='mx-1'>
+          <div className='contest-details-rules py-5 mb-5'>
+            <p className="relative self-stretch font-gilroybold text-[#B7C2FD] text-[18px] tracking-[0] leading-7 mb-3">WINNINGS:</p>
+            <ul className='mx-1 list-item'>
               {contest.warnings.map((item, index) => (
                 <React.Fragment key={index}>{renderContent(item)}</React.Fragment>
               ))}
