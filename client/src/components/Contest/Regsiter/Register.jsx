13e5498fc097096bd9bafc8bcfc2d336721d88a8
Hrithik <hrithik@gyangrove.co>
2024-10-22 01:26:08 +0530

    leader board component added

--- client/src/components/Contest/Regsiter/Register.jsx
+++ client/src/components/Contest/Regsiter/Register.jsx
@@ -13,7 +13,7 @@ import { renderContent } from "../../../utils/mapRichText";
 import DCButton from "../../button/DCButton";
 import cube from "../../../assets/cube.png";
 import line from "../../../assets/line.svg";
-
+import NewLeaderBoard from "../Leaderboard/NewLeaderBoard";
 
 export default function ContestRegsiter() {
   const { title } = useParams("title");
@@ -190,7 +190,8 @@ export default function ContestRegsiter() {
             <p className="my-2 text-[64px]  leading-tight text-overflow-ellipsis font-helvetica-neue-bold">Leaderboard</p>
           </div>
 
-          <Leaderboard data={leaderboard} />
+          {/* <Leaderboard data={leaderboard} /> */}
+          <NewLeaderBoard />
 
         </div>
       }
@@ -214,6 +215,7 @@ export default function ContestRegsiter() {
           />
         </div>
         <div className='col-span-2 md:pr-8 '>
+
 
           <div className='contest-details-title mb-5'>
             <div className="relative text-left w-fit mt-[-1.00px] 
